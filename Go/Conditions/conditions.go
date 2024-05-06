2fab13e8e0bd67659d5adbe35eecd97a9645d15d
heesane <tees3359@gmail.com>
2024-05-07 02:16:39 +0900

    feat : Go By Example Conditions

---
+++ Go/Conditions/conditions.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+
+	if 7%2 == 0 {
+		println("7 is even")
+	} else {
+		println("7 is odd")
+	}
+
+	if 8%4 == 0 {
+		println("8 is divisible by 4")
+	}
+
+	if 8%2 == 0 || 7%2 == 0 {
+		println("either 8 or 7 are even")
+	}
+
+	if num := 9; num < 0 {
+		println(num, "is negative")
+	} else if num < 10 {
+		println(num, "has 1 digit")
+	} else {
+		println(num, "has multiple digits")
+	}
+}
